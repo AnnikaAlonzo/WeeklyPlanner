ff2d625344d779564ff1e7e1c93875d0e9621cb0
Annika Dominique Alonzo <60319189+AnnikaAlonzo@users.noreply.github.com>
2021-04-04 22:23:32 +0800

    First commit
    Initial .js and .html files

---
+++ WeeklyPlanner/src/addExpense.js
@@ -0,0 +1,350 @@
+/*
+ *	This content is generated from the PSD File Info.
+ *	(Alt+Shift+Ctrl+I).
+ *
+ *	@desc 		
+ *	@file 		ui_ux_v1
+ *	@date 		1617544279960
+ *	@title 		Page 1
+ *	@author 	
+ *	@keywords 	
+ *	@generator 	Export Kit v1.3.figma
+ *
+*/
+
+var content_container = document.createElement("div");
+content_container.style.width = "595px";
+content_container.id = "content_container";
+content_container.style.position = "relative";
+content_container.style.display = "block";
+content_container.style.margin = "0 auto";
+document.body.appendChild(content_container);
+
+var _bg__add_expense = document.createElement("div");
+_bg__add_expense.id = "_bg__add_expense";
+_bg__add_expense.style.left = "0px";
+_bg__add_expense.style.top = "0px";
+_bg__add_expense.style.width = "595px";
+_bg__add_expense.style.height = "693px";
+_bg__add_expense.style.background = 'rgba(253.94,238.52,199.98,1)';
+
+content_container.appendChild(_bg__add_expense);
+
+var expense_name_ = document.createElement("div");
+expense_name_.innerHTML = "EXPENSE NAME:<br/>";
+expense_name_.style.textAlign = "left";
+expense_name_.id = "expense_name_";
+expense_name_.style.left = "38px";
+expense_name_.style.top = "63px";
+expense_name_.style.width = "256px";
+expense_name_.style.height = "62px";
+expense_name_.style.fontFamily = "Sansation";
+expense_name_.style.fontSize = "30px";
+expense_name_.style.overflow = "hidden";
+expense_name_.style.color = "#000000";
+
+content_container.appendChild(expense_name_);
+
+var rectangle_37 = document.createElement("div");
+rectangle_37.id = "rectangle_37";
+rectangle_37.style.left = "37px";
+rectangle_37.style.top = "115px";
+rectangle_37.style.width = "524px";
+rectangle_37.style.height = "48px";
+rectangle_37.style.border = "1px solid #000000";
+rectangle_37.style.background = 'rgba(255,255,255,1)';
+
+content_container.appendChild(rectangle_37);
+
+var rectangle_46 = document.createElement("div");
+rectangle_46.id = "rectangle_46";
+rectangle_46.style.left = "37px";
+rectangle_46.style.top = "252px";
+rectangle_46.style.width = "524px";
+rectangle_46.style.height = "48px";
+rectangle_46.style.border = "1px solid #000000";
+rectangle_46.style.background = 'rgba(255,255,255,1)';
+
+content_container.appendChild(rectangle_46);
+
+var day_of_the_week_ = document.createElement("div");
+day_of_the_week_.innerHTML = "DAY OF THE WEEK:";
+day_of_the_week_.style.textAlign = "left";
+day_of_the_week_.id = "day_of_the_week_";
+day_of_the_week_.style.left = "38px";
+day_of_the_week_.style.top = "337px";
+day_of_the_week_.style.width = "360px";
+day_of_the_week_.style.height = "64px";
+day_of_the_week_.style.fontFamily = "Sansation";
+day_of_the_week_.style.fontSize = "30px";
+day_of_the_week_.style.overflow = "hidden";
+day_of_the_week_.style.color = "#000000";
+
+content_container.appendChild(day_of_the_week_);
+
+var rectangle_39 = document.createElement("div");
+rectangle_39.id = "rectangle_39";
+rectangle_39.style.left = "37px";
+rectangle_39.style.top = "389px";
+rectangle_39.style.width = "68px";
+rectangle_39.style.height = "55px";
+rectangle_39.style.border = "1px solid #000000";
+rectangle_39.style.background = 'rgba(255,255,255,1)';
+
+content_container.appendChild(rectangle_39);
+
+var rectangle_40 = document.createElement("div");
+rectangle_40.id = "rectangle_40";
+rectangle_40.style.left = "433px";
+rectangle_40.style.top = "389px";
+rectangle_40.style.width = "68px";
+rectangle_40.style.height = "55px";
+rectangle_40.style.border = "1px solid #000000";
+rectangle_40.style.background = 'rgba(255,255,255,1)';
+
+content_container.appendChild(rectangle_40);
+
+var rectangle_41 = document.createElement("div");
+rectangle_41.id = "rectangle_41";
+rectangle_41.style.left = "367px";
+rectangle_41.style.top = "389px";
+rectangle_41.style.width = "68px";
+rectangle_41.style.height = "55px";
+rectangle_41.style.border = "1px solid #000000";
+rectangle_41.style.background = 'rgba(255,255,255,1)';
+
+content_container.appendChild(rectangle_41);
+
+var rectangle_42 = document.createElement("div");
+rectangle_42.id = "rectangle_42";
+rectangle_42.style.left = "301px";
+rectangle_42.style.top = "389px";
+rectangle_42.style.width = "68px";
+rectangle_42.style.height = "55px";
+rectangle_42.style.border = "1px solid #000000";
+rectangle_42.style.background = 'rgba(255,255,255,1)';
+
+content_container.appendChild(rectangle_42);
+
+var rectangle_43 = document.createElement("div");
+rectangle_43.id = "rectangle_43";
+rectangle_43.style.left = "235px";
+rectangle_43.style.top = "389px";
+rectangle_43.style.width = "68px";
+rectangle_43.style.height = "55px";
+rectangle_43.style.border = "1px solid #000000";
+rectangle_43.style.background = 'rgba(255,255,255,1)';
+
+content_container.appendChild(rectangle_43);
+
+var rectangle_44 = document.createElement("div");
+rectangle_44.id = "rectangle_44";
+rectangle_44.style.left = "169px";
+rectangle_44.style.top = "389px";
+rectangle_44.style.width = "68px";
+rectangle_44.style.height = "55px";
+rectangle_44.style.border = "1px solid #000000";
+rectangle_44.style.background = 'rgba(255,255,255,1)';
+
+content_container.appendChild(rectangle_44);
+
+var rectangle_45 = document.createElement("div");
+rectangle_45.id = "rectangle_45";
+rectangle_45.style.left = "103px";
+rectangle_45.style.top = "389px";
+rectangle_45.style.width = "68px";
+rectangle_45.style.height = "55px";
+rectangle_45.style.border = "1px solid #000000";
+rectangle_45.style.background = 'rgba(255,255,255,1)';
+
+content_container.appendChild(rectangle_45);
+
+var expense_amount_ = document.createElement("div");
+expense_amount_.innerHTML = "EXPENSE AMOUNT:";
+expense_amount_.style.textAlign = "left";
+expense_amount_.id = "expense_amount_";
+expense_amount_.style.left = "38px";
+expense_amount_.style.top = "200px";
+expense_amount_.style.width = "360px";
+expense_amount_.style.height = "62px";
+expense_amount_.style.fontFamily = "Sansation";
+expense_amount_.style.fontSize = "30px";
+expense_amount_.style.overflow = "hidden";
+expense_amount_.style.color = "#000000";
+
+content_container.appendChild(expense_amount_);
+
+var m = document.createElement("div");
+m.innerHTML = "M";
+m.style.textAlign = "center";
+m.id = "m";
+m.style.left = "44px";
+m.style.top = "397px";
+m.style.width = "58px";
+m.style.height = "61px";
+m.style.fontFamily = "Sansation";
+m.style.fontSize = "30px";
+m.style.textShadow = "0px 0px 1px #000000";
+m.style.overflow = "hidden";
+m.style.color = "#E1DDDD";
+
+content_container.appendChild(m);
+
+var m_ek1 = document.createElement("div");
+m_ek1.innerHTML = "M";
+m_ek1.style.textAlign = "center";
+m_ek1.id = "m_ek1";
+m_ek1.style.left = "44px";
+m_ek1.style.top = "397px";
+m_ek1.style.width = "58px";
+m_ek1.style.height = "61px";
+m_ek1.style.fontFamily = "Sansation";
+m_ek1.style.fontSize = "30px";
+m_ek1.style.textShadow = "0px 0px 1px #000000";
+m_ek1.style.overflow = "hidden";
+m_ek1.style.color = "#E1DDDD";
+
+content_container.appendChild(m_ek1);
+
+var su = document.createElement("div");
+su.innerHTML = "Su";
+su.style.textAlign = "center";
+su.id = "su";
+su.style.left = "434px";
+su.style.top = "397px";
+su.style.width = "71px";
+su.style.height = "61px";
+su.style.fontFamily = "Sansation";
+su.style.fontSize = "30px";
+su.style.textShadow = "0px 0px 1px #000000";
+su.style.overflow = "hidden";
+su.style.color = "#E1DDDD";
+
+content_container.appendChild(su);
+
+var s = document.createElement("div");
+s.innerHTML = "S";
+s.style.textAlign = "center";
+s.id = "s";
+s.style.left = "374px";
+s.style.top = "397px";
+s.style.width = "58px";
+s.style.height = "61px";
+s.style.fontFamily = "Sansation";
+s.style.fontSize = "30px";
+s.style.textShadow = "0px 0px 1px #000000";
+s.style.overflow = "hidden";
+s.style.color = "#E1DDDD";
+
+content_container.appendChild(s);
+
+var f = document.createElement("div");
+f.innerHTML = "F";
+f.style.textAlign = "center";
+f.id = "f";
+f.style.left = "308px";
+f.style.top = "397px";
+f.style.width = "58px";
+f.style.height = "61px";
+f.style.fontFamily = "Sansation";
+f.style.fontSize = "30px";
+f.style.textShadow = "0px 0px 1px #000000";
+f.style.overflow = "hidden";
+f.style.color = "#E1DDDD";
+
+content_container.appendChild(f);
+
+var h = document.createElement("div");
+h.innerHTML = "H";
+h.style.textAlign = "center";
+h.id = "h";
+h.style.left = "242px";
+h.style.top = "397px";
+h.style.width = "58px";
+h.style.height = "61px";
+h.style.fontFamily = "Sansation";
+h.style.fontSize = "30px";
+h.style.textShadow = "0px 0px 1px #000000";
+h.style.overflow = "hidden";
+h.style.color = "#E1DDDD";
+
+content_container.appendChild(h);
+
+var w = document.createElement("div");
+w.innerHTML = "W";
+w.style.textAlign = "center";
+w.id = "w";
+w.style.left = "176px";
+w.style.top = "397px";
+w.style.width = "58px";
+w.style.height = "61px";
+w.style.fontFamily = "Sansation";
+w.style.fontSize = "30px";
+w.style.textShadow = "0px 0px 1px #000000";
+w.style.overflow = "hidden";
+w.style.color = "#E1DDDD";
+
+content_container.appendChild(w);
+
+var t = document.createElement("div");
+t.innerHTML = "T";
+t.style.textAlign = "center";
+t.id = "t";
+t.style.left = "110px";
+t.style.top = "397px";
+t.style.width = "58px";
+t.style.height = "61px";
+t.style.fontFamily = "Sansation";
+t.style.fontSize = "30px";
+t.style.textShadow = "0px 0px 1px #000000";
+t.style.overflow = "hidden";
+t.style.color = "#E1DDDD";
+
+content_container.appendChild(t);
+
+var add_schedule = document.createElement("div");
+add_schedule.id = "add_schedule";
+add_schedule.style.width = "294px";
+add_schedule.style.height = "67px";
+add_schedule.style.left = "151px";
+add_schedule.style.top = "590px";
+add_schedule.style.position = "absolute";
+content_container.appendChild(add_schedule);
+
+var rectangle_15 = document.createElement("div");
+rectangle_15.id = "rectangle_15";
+rectangle_15.style.left = "0px";
+rectangle_15.style.top = "0px";
+rectangle_15.style.width = "296px";
+rectangle_15.style.height = "69px";
+rectangle_15.style.borderRadius = "50px";
+rectangle_15.style.border = "1px solid #000000";
+rectangle_15.style.background = 'rgba(233.75,233.75,233.75,1)';
+
+add_schedule.appendChild(rectangle_15);
+
+var submit = document.createElement("div");
+submit.innerHTML = "SUBMIT";
+submit.style.textAlign = "center";
+submit.id = "submit";
+submit.style.left = "44px";
+submit.style.top = "14px";
+submit.style.width = "212px";
+submit.style.height = "63.5px";
+submit.style.fontFamily = "Sansation";
+submit.style.fontSize = "35px";
+submit.style.overflow = "hidden";
+submit.style.color = "#000000";
+
+add_schedule.appendChild(submit);
+
+var image_7 = document.createElement("img");
+image_7.id = "image_7";
+image_7.style.left = "543px";
+image_7.style.top = "11px";
+image_7.style.width = "43px";
+image_7.style.height = "43px";
+image_7.src = "skins/image_7.png";
+
+content_container.appendChild(image_7);
+
